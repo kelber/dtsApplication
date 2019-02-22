988331cf61adc5d257577e3f71030eeb36638d31
Kelber <kelberstuchi@gmail.com>
2019-02-22 15:44:23 -0300

    solve conflit nrwl

--- apps/dtsapp/src/app/app.module.ts
+++ apps/dtsapp/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NxModule } from '@nrwl/nx';
-<<<<<<< HEAD
-import { MyMaterialModuleModule } from './my-material-module/my-material-module.module';
-=======
 import { RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
  
->>>>>>> 2-materialStyle
 
 @NgModule({
   declarations: [
@@ -21,14 +17,10 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     NxModule.forRoot(),
-<<<<<<< HEAD
-    MyMaterialModuleModule
-=======
     RouterModule,
     ReactiveFormsModule,
     FormsModule,
     HomeModule
->>>>>>> 2-materialStyle
   ],
   providers: [],
   bootstrap: [AppComponent]
